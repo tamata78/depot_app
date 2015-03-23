8aa6d890f1cead8b83acf0f73e2340dbae11fc8b
shuta tezuka <deveenjhalf7@gmail.com>
2015-03-23 23:34:22 +0900

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  #各項目の必須チェック
+  validates :title, :description, :image_url, presence: true
+  #数値かつ指定数値以上かをチェック
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  #他の商品名と重複しない名称かをチェック
+  validates :title, uniqueness: true
+  #適切な画像のURLかをチェック
+  validates :image_url, allow_blank: true, format: {
+    with: %r{.(gif|jpg|png)\z}i,
+    message: 'はGIF, JPG, PNG画像のURLでなければなりません'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,72 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "product attribute must not be empty" do
+    product = Product.new
+    # productモデルが妥当でないかをチェック
+    assert product.invalid?
+    # ハッシュで指定した項目のエラー有無チェック
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                                         description: "yyy",
+                                         image_url: "zzz.jpg")
+    # 価格が-1の場合,エラー
+    product.price = -1;
+    assert product.invalid?
+    # joinはエラーが複数存在した場合、;区切りで結合するメソッド
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    # 価格が0の場合,エラー
+    product.price = 0;
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    # 価格が1の場合,正常
+    product.price = 1;
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "My Book Title",
+                                         description: "yyy",
+                                         price: 1,
+                                         image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+                    http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a uniq title" do
+        #products(:ruby)においてフィクスチャにアクセス
+        product = Product.new(title: products(:ruby).title,
+                                         description: "yyy",
+                                         price: 1,
+                                         image_url: "fred.jpg")
+        assert !product.save
+        assert_equal I18n.translate('activerecord.errors.message.taken'),
+                            product.errors[:title].join('; ')
+  end
+
 end
+
+
